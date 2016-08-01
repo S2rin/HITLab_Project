e054f9a21676dc542ff12fb796280cb3514002df
whitehobbit <limhabin@gmail.com>
2016-08-01 12:37:06 +0900

    초기 세팅

---
+++ src/main/java/kr/ac/gachon/hitlab/FhirWebClientApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.gachon.hitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirWebClientApplication.class, args);
+	}
+}
