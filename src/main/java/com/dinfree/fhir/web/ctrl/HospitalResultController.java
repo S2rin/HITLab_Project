a56441c482ac7e73463bf02a027e8fcba0a645f0
whitehobbit <limhabin@gmail.com>
2016-08-10 23:31:22 +0900

    DTO update(LoincCode, Interpretation etc) Web static update

--- src/main/java/com/dinfree/fhir/web/ctrl/HospitalResultController.java
+++ src/main/java/com/dinfree/fhir/web/ctrl/HospitalResultController.java
@@ -1,16 +1,15 @@
 package com.dinfree.fhir.web.ctrl;
 
 import com.dinfree.fhir.web.boot.DevDataLoader;
-import com.dinfree.fhir.web.component.HospitalResult;
 import com.dinfree.fhir.web.domain.data.observation.GFObservation;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 /**
@@ -21,19 +20,21 @@ import java.util.ArrayList;
 public class HospitalResultController {
 
     @Autowired DevDataLoader ddl;
-    @Autowired HospitalResult hospitalResult;
 
     @RequestMapping("/hospitalResult")
     String index(@RequestParam(value = "hospital", required = false, defaultValue = "all")String hospital , Model model) {
+        ArrayList<HospitalResult> hospitalObs = new ArrayList<>();
         model.addAttribute("hospital", hospital);
+        model.addAttribute("hospitalObs", hospitalObs);
+
         if (hospital.equals("all")) {
-            hospitalResult.getHospitalResult().clear();
-            hospitalResult.getHospitalResult().addAll(ddl.getObservations());
+            ddl.getObservations().forEach(obs -> {
+                hospitalObs.add(new HospitalResult(obs));
+            });
         } else {
-            hospitalResult.getHospitalResult().clear();
             ddl.getObservations().forEach(obs -> {
                 if (hospital.equals(obs.getManagingOrganization())) {
-                    hospitalResult.getHospitalResult().add(obs);
+                    hospitalObs.add(new HospitalResult(obs));
                 }
             });
         }
@@ -41,8 +42,37 @@ public class HospitalResultController {
         return "hospitalResult";
     }
 
-    @ModelAttribute("hospitalResult")
-    public ArrayList<GFObservation> getHospitalResult() {
-        return hospitalResult.getHospitalResult();
+    @Data
+    class HospitalResult {
+        String effective;
+        String testItem;
+        String testValue;
+        String testUnit;
+        String standards;
+        String interpretation;
+        String managingOrganization;
+
+        public HospitalResult() {
+            this.effective = "";
+            this.testItem = "";
+            this.testValue = "";
+            this.testUnit = "";
+            this.standards = "";
+            this.interpretation = "";
+            this.managingOrganization = "";
+        }
+
+        HospitalResult(GFObservation obs) {
+            this();
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd a hh:mm:ss");
+            this.effective = dateFormat.format(obs.getEffective());
+            this.testItem = obs.getCode().getName();
+            this.testValue = Double.toString(obs.getValue());
+            this.testUnit = obs.getUnit();
+            this.standards = "";
+            this.interpretation = obs.getInterpretation().getName();
+            this.managingOrganization = obs.getManagingOrganization();
+        }
+
     }
 }
